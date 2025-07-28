346028ac711f8193ca9ff4d2c16c96532093c589
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-07-28 13:08:39 +0530

    Created main class

---
+++ Form Validator/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void  main(String[] args) {
+
+        // user inputs
+        String firstName = "Dinuka";
+        String lastName = "Rathnayake";
+        String email = "dinuka@test.com";
+        String phoneNumber = "0712345678";
+        String birthDay = "2000/01/01";
+        String nic = "123456789V";
+        String address = "No. 07, Temple Road, Colombo 07";
+        String password = "Test@123";
+        String confirmPassword = "Test@123";
+    }
+}
